6b70a721bcb4ddd4a0205c78a6853b9da4f48c12
Carlos Rocha <rochacarlos42@yahoo.com.br>
2025-06-08 21:03:43 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post("/usuarios", (req, res) => {
+    
+    users.push(req.body)
+
+    res.send("ok post")
+})
+
+app.get("/usuarios", (req, res) => {
+    res.json(users)
+})
+
+app.listen(3000)  
